c1f4345755f62d5526d15d35a0f40d9d8f2476b1
OsamaAteba77 <osamahateba@gmail.com>
2025-10-27 22:41:38 +0300

    Finalized Tailwind config and created Atomic Design folder structure.

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+  // This tells Tailwind to scan your Vue files for classes
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
